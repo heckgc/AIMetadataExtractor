d3bf1e98d19a026af703f31831dacebd21f41c0d
Heck <heckgc@gmail.com>
2025-01-09 14:09:11 +0000

    remove run and tests from install

--- install.bat
+++ install.bat
@@ -7,11 +7,3 @@ python -m venv .venv
 
 echo Installing dependencies...
 .venv\Scripts\pip install -r requirements.txt
-
-REM Run the application
-echo Starting the application...
-.venv\Scripts\python src\main.py
-
-REM Run tests
-echo Running tests...
-.venv\Scripts\python -m unittest discover -s tests -p "test_*.py"
